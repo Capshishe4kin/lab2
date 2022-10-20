8b0be8dfcb5f33410d24c8e582315336f5783a02
Capshishe4kin <5838141@stud.nau.edu.ua>
2022-10-20 21:26:25 +0300

    My second lab

---
+++ bv/lab1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+backup_files="/ubuntu/bv/boba"
+dest="/home/bv/backup"
+
+day=$(date +%A)
+hostname=$(hostname -s)
+archive_file="$hostname-$day.tgz"
+
+echo "Creating backup $backup_files to $dest/$archive_file"
+
+
+date
+
+
+
+
+
+tar czf $dest/$archive_file $backup_files
+
+
+
+echo "Back up finished"
+
